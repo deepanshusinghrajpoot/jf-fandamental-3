b79b3c5822172b1b1f2a02069bf7b16b65785309
deepanshusinghrajpoot <144131132+deepanshusinghrajpoot@users.noreply.github.com>
2024-09-24 10:27:12 +0530

    Add files via upload

---
+++ 06.js
@@ -0,0 +1,7 @@
+function reverseString(input) {
+    return input.split('').reverse().join('');
+  }
+  
+  const originalString = "Hello, world!";
+  const reversedString = reverseString(originalString);
+  console.log(reversedString);
